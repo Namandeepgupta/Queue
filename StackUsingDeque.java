0ae137232b7c0566f37e470a9b78e6bb27823b30
Namandeep gupta <iamnamandeepgupt@gmail.com>
2024-11-16 11:09:57 +0530

    Add files via upload

---
+++ StackUsingDeque.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class StackUsingDeque {
+    static class Stack{
+        static Deque<Integer> deque = new LinkedList<>();
+        public static boolean isEmpty(){
+            return deque.isEmpty();
+        }
+        public static void push(int data){
+            deque.addLast(data);
+        }
+        public static int pop(){
+            return deque.removeLast();
+        }
+        public static int peek(){
+            return deque.getLast();
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
